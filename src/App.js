5c6586e32b811d1da0cc8a7d2dab68b0b4cdfb30
Elias Ariel Valenzuela - XA55788 <eliasariel.valenzuela.contractor@gdevtools.com>
2022-01-31 22:36:03 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,6 +17,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossorigin="anonymous"
+          />
           Learn React
         </a>
       </header>
